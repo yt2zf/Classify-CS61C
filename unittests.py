1431d20e871941f9af55da7c7f7a78c870588387
Yanting Wan <studyYanting@163.com>
2024-12-04 12:54:15 +0800

    own test

--- unittests.py
+++ unittests.py
@@ -53,7 +53,7 @@ class TestRelu(unittest.TestCase):
         # load the test for relu.s
         t = AssemblyTest(self, "relu.s")
         # create an array in the data section
-        array0 = t.array([1, -2, 3, -4, 5, -6, 7, -8, 9])
+        array0 = t.array([1, -2, 3, -4, 5, -6, 7, -8, 9, (1<<31)-1, 1<<31])
         # load address of `array0` into register a0
         t.input_array("a0", array0)
         # set a1 to the length of our array
@@ -61,7 +61,7 @@ class TestRelu(unittest.TestCase):
         # call the `relu` function
         t.call("relu")
         # check that the array0 was changed appropriately
-        t.check_array(array0, [1, 0, 3, 0, 5, 0, 7, 0, 9])
+        t.check_array(array0, [1, 0, 3, 0, 5, 0, 7, 0, 9, (1<<31)-1, 0])
         # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
         t.execute()
 
@@ -92,10 +92,25 @@ class TestRelu(unittest.TestCase):
 
 
 class TestArgmax(unittest.TestCase):
+    def test_argmax_lastMax(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array0 = t.array([3, -42, 432, 7, -5, 6, 5, -114, 2, 432, 999])
+        # load address of the array into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of the array
+        t.input_scalar("a1", len(array0))
+        # call the `argmax` function
+        t.call("argmax")
+        # check that the register a0 contains the correct output
+        t.check_scalar("a0", 10)
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute()
+
     def test_argmax_standard(self):
         t = AssemblyTest(self, "argmax.s")
         # create an array in the data section
-        array0 = t.array([3, -42, 432, 7, -5, 6, 5, -114, 2])
+        array0 = t.array([3, -42, 432, 7, -5, 6, 5, -114, 2, 432])
         # load address of the array into register a0
         t.input_array("a0", array0)
         # set a1 to the length of the array
